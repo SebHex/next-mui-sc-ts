134ffc49deb3db14d2c243d0753024ff846ca0d2
Sebastian <seb@cixel.com>
2022-01-12 09:41:31 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
+const withTM = require('next-transpile-modules')([
+  '@mui/material',
+  '@mui/system'
+]);
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    styledComponents: true
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@mui/styled-engine': '@mui/styled-engine-sc'
+    };
+    return config;
+  }
+};
+
+module.exports = withTM(nextConfig);
