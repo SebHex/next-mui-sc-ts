a13f6197ccff1925d106165f05f47f9e372d447a
Sebastian <seb@cixel.com>
2022-01-12 09:42:16 +0800

    Add custom document

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import type { DocumentContext, DocumentInitialProps } from 'next/document'
+import Document from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+class CustomDocument extends Document {
+  public static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+}
+
+export default CustomDocument
